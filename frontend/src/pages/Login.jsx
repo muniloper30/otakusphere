31ce558ff27a50c2af578c1c761d3edccb453bbf
Victor Lorencio <victorlorencio@gmail.com>
2025-05-06 12:34:37 +0200

    Conectar login front al back e inicio de sesion correcto

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,42 @@
 import { Link } from "react-router-dom";
-
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 
 const Login = () => {
+
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+const navigate = useNavigate();
+
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  try {
+    const response = await axios.post("http://localhost:8080/usuarios/login", {
+      email,
+      password,
+    });
+
+    const { token, usuario } = response.data;
+
+    // Guardar en localStorage
+    localStorage.setItem("token", token);
+    localStorage.setItem("usuario", JSON.stringify(usuario));
+
+    // Redirigir
+    navigate("/perfil");
+
+  } catch (error) {
+    console.error("Error al iniciar sesión:", error);
+    alert("Credenciales incorrectas o usuario no registrado.");
+  }
+};
+
+
+
   return (
     <div className="relative h-screen w-full md:flex">
       {/* Imagen de fondo para móvil */}
@@ -22,7 +56,7 @@ const Login = () => {
         <h1 className="bg-gradient-to-r from-[#F166B4] to-[#1B9CF0] text-2xl font-bold mb-6 py-2 px-3 border rounded-md animate-impulse-rotation-left">
           Acceder
         </h1>
-        <form className="bg-gray-100 p-6 rounded shadow-md w-75 max-w-md animate-impulse-rotation-left">
+        <form onSubmit={handleSubmit} className="bg-gray-100 p-6 rounded shadow-md w-75 max-w-md animate-impulse-rotation-left">
           <div className="mb-4">
             <label
               className="block text-gray-800 text-sm font-bold mb-2"
@@ -45,8 +79,10 @@ const Login = () => {
               Correo electrónico
             </label>
             <input
-              type="text"
-              id="username"
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               placeholder="Introduce el correo de tu cuenta"
               className="bg-gradient-to-r from-[#F166B4] to-[#1B9CF0] shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
             />
@@ -61,6 +97,8 @@ const Login = () => {
             <input
               type="password"
               id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               placeholder="Escribe tu contraseña"
               className="bg-gradient-to-r from-[#F166B4] to-[#1B9CF0] shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
             />
