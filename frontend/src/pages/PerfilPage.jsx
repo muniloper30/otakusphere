6fc0f2275a152a7b0ba6a239abd8e8666717f879
Victor Lorencio <victorlorencio@gmail.com>
2025-05-18 17:57:53 +0200

    update responsive perfilpage.jsx

--- frontend/src/pages/PerfilPage.jsx
+++ frontend/src/pages/PerfilPage.jsx
@@ -3,7 +3,7 @@ import SidebarPerfil from "../components/perfil/SidebarPerfil";
 
 const PerfilPage = () => {
   return (
-    <div className="min-h-screen flex bg-gradient-to-r from-[#F166B4] to-[#1B9CF0]">
+    <div className="min-h-screen flex flex-col md:flex-row bg-gradient-to-r from-[#F166B4] to-[#1B9CF0]">
       <SidebarPerfil />
       <main className="flex-1 p-6">
         <Outlet />
